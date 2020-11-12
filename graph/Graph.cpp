099d749cfcba6befa9e22b2f2c2a48d01eda55a1
seanngpack <seanngpack@gmail.com>
2020-11-12 12:29:29 -0500

    fixed recursive dfs

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <set>
+#include <stack>
 
 struct Edge {
     int src;
@@ -44,6 +45,41 @@ public:
             }
         }
     }
+
+    // i can also try a slightly different algorithm that has a target.
+    // this one just searches everything
+    // https://leetcode.com/problems/all-paths-from-source-to-target/discuss/118647/C++-DFS-Recursive-Easy-to-Understand
+    void print_dfs_recursive(int node, std::set<int> &seen) {
+        seen.insert(node);
+
+        std::cout << node << " ";
+
+        for (int n : adj_list[node]) {
+            if (seen.find(n) == seen.end()) {
+                print_dfs_recursive(n, seen);
+            }
+        }
+    }
+
+    // print all the nodes in the graph using dfs
+    // DONT PUT SEEN BEFORE PUSHING TO CALL STACK
+    void print_dfs_iterative(int start) {
+        std::set<int> seen;
+        std::stack<int> stk;
+        stk.push(start);
+        seen.insert(start);
+        while (!stk.empty()) {
+            int top = stk.top();
+            std::cout << top << " ";
+            stk.pop();
+            for (int neighbor: adj_list[top]) {
+                if (seen.find(neighbor) == seen.end()) {
+//                    seen.insert(neighbor);
+                    stk.push(neighbor);
+                }
+            }
+        }
+    }
 };
 
 void print_graph(const Graph &graph, int n) {
@@ -57,6 +93,8 @@ void print_graph(const Graph &graph, int n) {
     }
 }
 
+
+
 int main() {
 //    std::vector<Edge> edges =
 //            {
@@ -69,7 +107,7 @@ int main() {
 //                    {5, 4}
 //            };
 
-    std::vector<Edge> edges =
+    std::vector<Edge> bfs_edges =
             {
                     {0, 1},
                     {0, 2},
@@ -77,19 +115,35 @@ int main() {
                     {2, 4}
             };
 
+    std::vector<Edge> dfs_edges = {
+            // Notice that node 0 is unconnected node
+            {1, 2}, {1, 7}, {1, 8}, {2, 3}, {2, 6}, {3, 4},
+            {3, 5}, {8, 9}, {8, 12}, {9, 10}, {9, 11}
+    };
 
 
-    // Number of nodes in the graph
-    int N = 6;
+    int bfs_N = 5;
+    int dfs_N = 13;
 
     // construct graph
-    Graph graph(edges, N);
+    Graph bfs_graph(bfs_edges, bfs_N);
+    Graph dfs_graph(dfs_edges, dfs_N);
 
     // print adjacency list representation of graph
 //    print_graph(graph, N);
 
     // do bfs
-    graph.print_bfs(0);
+    bfs_graph.print_bfs(0);
+
+    // do recursive dfs
+    std::cout << std::endl;
+    std::set<int> s;
+    dfs_graph.print_dfs_recursive(1, s);
+
+    // do iterative dfs, it uses a stack and prints in different order than recursive
+    std::cout << std::endl;
+    dfs_graph.print_dfs_iterative(1);
+
 
     return 0;
 }
